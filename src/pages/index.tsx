d4b2e119904dac7b3a30519e541e614fe4b4962e
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-06-13 14:40:09 -0300

    fix: hydration error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,23 +28,17 @@ export default function Home({ products }: HomeProps) {
   });
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
+    <HomeContainer ref={ sliderRef } className="keen-slider">
       {products.map((product) => {
         return (
           <Link href={`/product/${product.id}`} key={product.id} >
             <Product className="keen-slider__slide" >
+              
               <Image src={product.imageUrl} width={520} height={480} alt={""} />
-
+              
               <footer>
                 <strong>{product.name}</strong>
-                <span>
-                  {
-                  new Intl.NumberFormat('pt-br', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  }).format(product.price)
-                  }
-                </span>
+                <span>{product.price}</span>
               </footer>
             </Product>
           </Link>
