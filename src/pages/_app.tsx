977b5f60480746ff0683ac39b01919bf237cd50d
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-06-14 16:20:38 -0300

    feat: link to home page in header logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { globalStyles } from '../styles/global'
 import { Container, Header } from '../styles/pages/app'
 import LogoImg from '../../public/logo.svg'
 import Image from 'next/image'
-
+import Link from 'next/link'
 
 globalStyles()
 
@@ -12,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
   <Container>
     <Header>
-      <Image src={LogoImg} alt="" />
+      <Link href="/" >
+        <Image src={LogoImg} alt="" />
+      </Link>
     </Header>
 
     <Component {...pageProps} />
